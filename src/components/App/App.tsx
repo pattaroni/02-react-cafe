e7fd3093292f5532b8742f63ca225dafb3977fa6
Oleksii Kyryk <kirikoleksiy112@gmail.com>
2025-09-15 20:54:50 +0300

    update

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import CafeInfo from "../CafeInfo/CafeInfo";
+import css from "./App.module.css";
+
+export default function App() {
+  return (
+    <>
+      <div className={css.app}>
+        <CafeInfo />
+      </div>
+    </>
+  );
+}
